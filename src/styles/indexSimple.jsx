6d454c282b7bcadea350b886952e1971d5ac5b02
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-09-16 13:10:56 -0700

    Create indexSimple.jsx
    simple styling vs

---
+++ src/styles/indexSimple.jsx
@@ -0,0 +1,16 @@
+.list-group-item {
+    display: inline-flex;
+} 
+.list-group-item:hover .x-container {
+    display: inline-flex;
+}
+.x-container {
+    display: none;
+    /* position: absolute; */
+    align-items: center;
+    margin-left: 20px;
+}
+.x-container:hover {
+    color: red;
+    cursor: pointer;
+}
